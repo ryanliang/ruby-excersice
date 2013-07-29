7248b6cc73cfb62026568ba79b2a825ba63743bc
Ryan Liang <ryanliang@gmail.com>
2013-07-28 23:04:59 -0400

    make sure pos is within boundary before putting locker there

--- locker_room/wall.rb
+++ locker_room/wall.rb
@@ -24,7 +24,7 @@ module Gym
     end
 
     def put_locker_at(locker, x, y)
-      @lockers[x][y] = locker if vacant_at?(x, y)
+      @lockers[x][y] = locker if position_within_boundary(x, y) && vacant_at?(x, y)
     end
 
     def owner_name_at(x, y)
